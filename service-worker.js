2743f97073fa7a81da8a56511171ef7f077157aa
bajajneha27 <bajajneha27@users.noreply.github.com>
2020-06-15 13:54:10 +0000

    Deploying to gh-pages from  @ 37aca5d195fe2e6d328843bd4522994f5fd25c90 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d860e6f9209cbf0c003d67443b6ad899.js"
+  "/precache-manifest.bfa3e621459d5e168cb1814700447815.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
