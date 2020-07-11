f897ddfa028934c3eb0c55341f73cdcbf651e043
Mayank Bhagya <mayankbhagya@gmail.com>
2020-07-11 19:05:04 +0530

    Cleaning footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,18 +7,29 @@ function Footer(props) {
 
   return (
     <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-      <h3>Proof of concept app based on covid19india.org</h3>
       <h5>{t('We stand with everyone fighting on the frontlines')}</h5>
 
-      <div className="link">
-        <a
-          href="https://covid19india.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          covid19india
-        </a>
-      </div>
+      <a
+        href="https://www.facebook.com/indiacovidseva"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button facebook"
+        style={{justifyContent: 'center'}}
+      >
+        <Icon.Facebook />
+        <span>{t('View updates on Facebook')}</span>
+      </a>
+
+      <a
+        href="https://twitter.com/indiacovidseva"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="button twitter"
+        style={{justifyContent: 'center'}}
+      >
+        <Icon.Twitter />
+        <span>{t('Follow us on Twitter')}</span>
+      </a>
 
       <a
         href="https://github.com/VICS-CORE/covid19india-react"
@@ -30,37 +41,8 @@ function Footer(props) {
         <span>{t('Open Sourced on GitHub')}</span>
       </a>
 
-      <a
-        className="button excel"
-        href="http://patientdb.covid19india.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Icon.Database />
-        <span>{t('Crowdsourced Patient Database')}</span>
-      </a>
+      <h5>Based on covid19india.org</h5>
 
-      <a
-        href="https://twitter.com/indiacovidseva"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button twitter"
-        style={{justifyContent: 'center'}}
-      >
-        <Icon.Twitter />
-        <span>{t('View updates on Twitter')}</span>
-      </a>
-
-      <a
-        href="https://www.facebook.com/indiacovidseva"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="button facebook"
-        style={{justifyContent: 'center'}}
-      >
-        <Icon.Facebook />
-        <span>{t('Like us on Facebook')}</span>
-      </a>
     </footer>
   );
 }
